07bdf0c10be2d4bc765aece62070d3d89fe60dda
shakerbaker78 <bakeamym@yahoo.com>
2022-05-21 20:39:24 -0600

    swagger

---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Armada API',
+    description: 'Armada API',
+  },
+
+  // host: 'localhost:3000',
+  // schemes: ['http'],
+
+  host: 'https://amy-baker-armada.herokuapp.com',
+  schemes: ['https'],
+
+  
+};
+
+const outputFile = '.swagger-output.json';
+const endpointsFiles = ['./routes/index.js'];
+
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
