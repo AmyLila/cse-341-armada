b59e5d77cc254db2b13125cd757ae8d6f8828550
shakerbaker78 <bakeamym@yahoo.com>
2022-05-21 21:32:36 -0600

    swagger

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 routes.use('/', require('./home'));
 routes.use('/users', require('./users'));
-// routes.use('/api-docs', require('./swagger'));
+routes.use('/api-docs', require('./swagger'));
 
 
 
