c1488d3f59ac73dc6537f6ec0a6da9ba19438712
shakerbaker78 <bakeamym@yahoo.com>
2022-06-17 19:52:34 -0600

    added code to hash the password

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -24,5 +25,29 @@ const userSchema = new mongoose.Schema({
     }
 
 });
+
+//Code to hash the password.
+userSchema.pre("save", function (next) {
+    const user = this
+  
+    if (this.isModified("password") || this.isNew) {
+      bcrypt.genSalt(10, function (saltError, salt) {
+        if (saltError) {
+          return next(saltError)
+        } else {
+          bcrypt.hash(user.password, salt, function(hashError, hash) {
+            if (hashError) {
+              return next(hashError)
+            }
+  
+            user.password = hash
+            next()
+          })
+        }
+      })
+    } else {
+      return next()
+    }
+  })
 
 module.exports = mongoose.model('User', userSchema);
